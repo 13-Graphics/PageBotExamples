837952f5bd4c0d53b60f394d85be0aa127cbc2ad
petrvanblokland <buro@petr.com>
2018-09-30 23:06:50 +0200

    Working on Berthold specimen

--- Typespecimens/BertholdTypeSpecimen.py
+++ Typespecimens/BertholdTypeSpecimen.py
@@ -34,7 +34,7 @@ from pagebot.contributions.filibuster.blurb import Blurb
 
 from pagebot.toolbox.transformer import path2FontName # Convenient CSS color to PageBot color conversion
 from pagebot.toolbox.hyphenation import wordsByLength # Use English hyphenation dictionary as word selector
-from pagebot.toolbox.units import inch, pt, em
+from pagebot.toolbox.units import inch, pt, em, p
 from pagebot.toolbox.color import color, blackColor
 
 context = getContext()
@@ -48,31 +48,29 @@ sampleFont = findFont('Upgrade-Bold')
 blurb = Blurb()
     
 # Basic page metrics.
-U = pt(8) # Page layout units, to unite baseline grid and gutter.
+U = pt(9) # Page layout units, to unite baseline grid and gutter.
 W, H = pt(590, 842) # Copy size from original Berthold specimen scan.
 # Hard coded padding sizes derived from the scan.
-PT, PR, PB, PL = PADDING = pt(90, 40, 40, 40) # Page padding top, right, bottom, left
+PADDING_LEFT = pt(90, 42, 42, 40) # Page padding top, right, bottom, left
+PADDING_RIGHT = pt(90, 40, 42, 42) # Page padding top, right, bottom, left
 L = 2*U # Baseline leading
-G = 3*U # Default gutter = 2pace between the columns
-
+GUTTER = U # Default gutter = 2pace between the columns
 # Hard coded column sizes derived from the scan.
-C1, C2, C3 = (112, 300, 112)
+CW = (W - PADDING_LEFT[3] - PADDING_LEFT[1] - 2*GUTTER)/3
 # Construct the grid pattern. 
 # Last value None means that there is no gutter running inside the right padding.
-GRID_X = ((C1, G), (C2, G), (C3, G))
-GRID_Y = ((H - PT - PB, G),)
+GRID_X = ((CW, GUTTER), (CW, GUTTER), (CW, 0))
+
+CC = color(1, 1, 1, 0.6)
 
 # Classic Berthold type specimen
 # Path to the scan, used to show at first page of this document.
 BERTHOLD_PATH = 'resources/BertholdBodoniOldFace#152.pdf'
 
-labelStyle = dict(font=sampleFont, fontSize=7, leading=em(1), paragraphTopSpacing=4, paragraphBottomSpacing=-10)
-
 # Sample glyphs set in bottom right frame. Automatic add a spacing between all characters.
 GLYPH_SET = ' '.join(u'ABCDEFGHIJKLMNOPQRSTUVWXYZ&$1234567890abcdefghijklmnopqrstuvwxyz.,-‘:;!?')
 
 # Export in _export folder that does not commit in Git. Force to export PDF.
-DO_OPEN = False
 if SHOW_GRID:
     EXPORT_PATH_PDF = '_export/Berthold-Grid.pdf' 
     EXPORT_PATH_PNG = '_export/Berthold-Grid.png' 
@@ -80,22 +78,9 @@ else:
     EXPORT_PATH_PDF = '_export/Berthold-%s.pdf' % FAMILIES[0].name 
     EXPORT_PATH_PNG = '_export/Berthold-%s.png' % FAMILIES[0].name 
 
-MAX_PAGES = 5
-
 # Some parameters from the original book
 PAPER_COLOR = color(rgb=0xFEFEF0) # Approximation of paper color of original specimen.
 RED_COLOR = color(rgb=0xAC1E2B) # Red color used in the original specimen
-
-# Get the dictionary of English ("en" is default language), other choice is Dutch ("nl").
-# Danish could be made available for PageBot if requested.
-# Other hyphenation tables are appreciated to be added to PageBot.
-# WORDS key is the word length in character count and the values are lists words of
-# equal amount of characters.
-# In case there are no words available for a given length, longer words will be clipped.
-LANGUAGE = 'nl' #'en'
-WORDS = wordsByLength(LANGUAGE)
-SHORT_WORDS = WORDS[3]+WORDS[4]+WORDS[3]+WORDS[4]+WORDS[3]+WORDS[4]+WORDS[5]+WORDS[6]
-shuffle(SHORT_WORDS)
 
 def getCapitalizedWord(l):
     u"""Select a random word from the hyphenation dictionary for this language."""
@@ -121,15 +106,25 @@ def buildSpecimenPages(doc, family, pn):
     return pn
    
 def makePage1(page, font):
+    style = dict(font=font, fontSize=pt(24))
+    bs = context.newString(font.info.familyName[:2].capitalize(), style=style)
+    newTextBox(bs, parent=page, conditions=[Left2Left(), Top2TopSide()], fill=(1, 0, 0))
+
     border = dict(stroke=blackColor, strokeWidth=pt(0.5))
-    r = newRect(parent=page, conditions=[Fit()], padding=0)
-    r.showPadding = True
+    r = newRect(parent=page, conditions=[Fit()], padding=0, fill=CC)
     
     fullName = '%s %s' % (font.info.familyName, font.info.styleName)
     style = dict(font=font, fontSize=pt(24), xTextAlign=CENTER)
     bs = context.newString(fullName.upper(), style=style)
     newTextBox(bs, parent=r, borderTop=border, borderBottom=border, margin=0, padding=0,
+        h=34,
         conditions=[Left2Left(), Top2Top(), Fit2Width()])
+    
+    c1 = newRect(parent=r, w=CW, conditions=[Left2Left(), Float2Top(), Fit2Height()], fill=CC)
+    c2 = newRect(parent=r, w=CW, conditions=[Left2Col(2), Float2Top(), Fit2Height()], fill=CC)
+    c3 = newRect(parent=r, w=CW, conditions=[Left2Col(3), Float2Top(), Fit2Height()], fill=CC)
+    
+    r.solve()
     
 def makePage2(page, font):
     border = dict(stroke=blackColor, strokeWidth=pt(0.5))
@@ -146,45 +141,37 @@ def makeDocument(font):
 
     # Build 4 pages, two for the original scan, the two for the generated version.
     doc = Document(w=W, h=H, title='Variable Font Sample Page', originTop=False, 
-        autoPages=4, context=context, gridX=GRID_X, gridY=GRID_Y)
+        autoPages=4, context=context, gridX=GRID_X)
     
     # Get default view from the document and set the viewing parameters.
     view = doc.view
     view.padding = 0 # For showing cropmarks and such, make > mm(20) or inch(1).
     view.showPageCropMarks = True # Won't show if there is not padding in the view.
-    view.showPageFrame = SHOW_FRAMES # No frame in case PAPER_COLOR exists to be shown.
-    view.showPagePadding = SHOW_FRAMES # No frame in case PAPER_COLOR exists to be shown.
-    view.showPageRegistrationMarks = True
+    view.showFrame = SHOW_FRAMES # No frame in case PAPER_COLOR exists to be shown.
+    view.showPadding = SHOW_FRAMES # No frame in case PAPER_COLOR exists to be shown.
+    view.showRegistrationMarks = True
     view.showGrid = SHOW_GRID # Show GRID_X lines
     view.showPageNameInfo = True # Show file name and date of the document
     view.showTextOverflowMarker = False # Don't show marker in case Filibuster blurb is too long.
 
     for pn in range(1, 3):
         page = doc[pn]
-        page.ch = 0 # No vertical grid
-        page.padding = PADDING
-        page.gridX = GRID_X
-        newRect(parent=page, fill=PAPER_COLOR, conditions=[Fit()])
+        newRect(parent=page, fill=PAPER_COLOR, x=0, y=0, w=page.w, h=page.h)
+        page.padding = {True: PADDING_LEFT, False: PADDING_RIGHT}[page.isLeft]        
         newImage(BERTHOLD_PATH, x=0, y=0, w=W, index=pn, parent=page)
 
     page = doc[3]
-    page.ch = 0 # No vertical grid
-    page.padding = PADDING
-    page.gridX = GRID_X
-    newRect(parent=page, fill=PAPER_COLOR, conditions=[Fit()])
     # During development, draw the template scan as background
     # Set z-azis != 0, to make floating elements not get stuck at the background
+    page.padding = {True: PADDING_LEFT, False: PADDING_RIGHT}[page.isLeft]        
     if SHOW_TEMPLATE:
         newImage(BERTHOLD_PATH, x=0, y=0, z=-10, w=W, index=1, parent=page)
     makePage1(page, sampleFont)
 
     page = doc[4]
-    page.ch = 0 # No vertical grid
-    page.padding = PADDING
-    page.gridX = GRID_X
-    newRect(parent=page, fill=PAPER_COLOR, conditions=[Fit()])
     # During development, draw the template scan as background
     # Set z-azis != 0, to make floating elements not get stuck at the background
+    page.padding = {True: PADDING_LEFT, False: PADDING_RIGHT}[page.isLeft]        
     if SHOW_TEMPLATE:
         newImage(BERTHOLD_PATH, x=0, y=0, z=-10, w=W, index=2, parent=page)
     makePage2(page, sampleFont)
@@ -195,7 +182,5 @@ def makeDocument(font):
 
 doc = makeDocument(sampleFont)
 doc.export(EXPORT_PATH_PDF) 
-if DO_OPEN:
-    os.system(u'open "%s"' % EXPORT_PATH)
   
 print('Done')
